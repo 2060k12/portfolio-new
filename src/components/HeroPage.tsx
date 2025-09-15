48ec3fb38f5eeeef02a54a50c87385e3d14c04a6
Pranish <iampranish@outlook.com>
2025-09-15 18:37:48 +1000

    deploy try 1

--- src/components/HeroPage.tsx
+++ src/components/HeroPage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const HeroPage = () => {
   return (
     <div className="page1">
--- src/components/TechIconPage.tsx
+++ src/components/TechIconPage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const TechIconPage = () => {
   return (
     <div className="page2">
